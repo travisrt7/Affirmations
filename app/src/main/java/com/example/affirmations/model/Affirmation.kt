be822748b35af20cacd01049352037c3e311ca9f
Travis Thompson <travisrt7@gmail.com>
2023-10-01 16:37:52 -0400

    Initial commit

---
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -0,0 +1,8 @@
+package com.example.affirmations.model
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    val stringResourceId: Int,
+    val imageResourceId: Int
+)
